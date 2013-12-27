8f6f973a3ebdbf391e7f48b1f68ff3d09fe153db
Stefan Lau <github@stefanlau.com>
2013-12-27 17:57:14 +0100

    trigger start event on STARTPROC

--- test/specs/DataHandlerSpec.js
+++ test/specs/DataHandlerSpec.js
@@ -1,1 +1,32 @@
+'use strict';
 
+var through = require('through'),
+    sinon = require('sinon'),
+    chai = require('chai'),
+    expect = chai.expect,
+    sinonChai = require('sinon-chai'),
+
+    DataHandler = require('../../lib/DataHandler');
+
+chai.use(sinonChai);
+
+describe('DataHandler', function () {
+    var dataHandler,
+        inputStream;
+
+    beforeEach(function () {
+        inputStream = through().pause();
+        dataHandler = new DataHandler(inputStream);
+    });
+
+    it('should trigger the start event on STARTPROC', function (done) {
+        inputStream.queue('<STARTPROC/>\n').end();
+        dataHandler.on('event', function (event) {
+            expect(event).to.deep.equal({
+                name: 'start'
+            });
+            done();
+        });
+        inputStream.resume();
+    });
+});
