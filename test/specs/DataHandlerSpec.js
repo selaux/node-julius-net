5db0b065e27dc519859489711fd1b4c650e46456
Stefan Lau <github@stefanlau.com>
2013-12-27 18:18:08 +0100

    trigger recognition event on RECOGOUT

--- test/specs/DataHandlerSpec.js
+++ test/specs/DataHandlerSpec.js
@@ -29,4 +29,30 @@ describe('DataHandler', function () {
         });
         inputStream.resume();
     });
+
+    it('should trigger the recognition event on RECOGOUT', function (done) {
+        var xml =   '<RECOGOUT>' +
+                        '<SHYPO RANK="1" SCORE="-3549.97">' +
+                            '<WHYPO WORD="Some" CLASSID="something" PHONE="p h o n e 1" CM="0.914"/>' +
+                            '<WHYPO WORD="Words" CLASSID="else" PHONE="p h o n e 2" CM="1"/>' +
+                        '</SHYPO>' +
+                    '</RECOGOUT>';
+        inputStream.queue(xml).end();
+        dataHandler.on('event', function (event) {
+            expect(event).to.deep.equal({
+                name: 'recognition',
+                data: [
+                    {
+                        score: -3549.97,
+                        words: [
+                            { word: 'Some', phone: 'p h o n e 1', cm: 0.914 },
+                            { word: 'Words', phone: 'p h o n e 2', cm: 1 }
+                        ]
+                    }
+                ]
+            });
+            done();
+        });
+        inputStream.resume();
+    });
 });
